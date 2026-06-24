11756507ea3d2d666289222eceaeeea064125d3f
jerryhp <jerryhp@DESKTOP-C85HKKA.localdomain>
2026-06-25 00:08:35 +0800

    completed ex00, continuing ex01

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+	result = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other) {
+	result = other.result;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) { 
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other) {
+		this->result = other.result;
+	}
+	return (*this);
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits(void) {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (result);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	this->result = raw;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+	private:
+		int				result;
+	static const int	frac_bits = 8;
+	public:
+		Fixed();
+		Fixed(const int num);
+		Fixed(const float numb);
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+		int		getRawBits(void);
+		void	setRawBits(int const raw);
+
+};
+#endif
